4f44fa47749e391ec03575e75cf43bf6863d924c
Dave Compton <davecompton7@gmail.com>
2016-09-23 21:59:10 +0000

    introduced clicker function

--- Main.hs
+++ Main.hs
@@ -2,19 +2,11 @@
 {-# LANGUAGE RecursiveDo #-}
 import Reflex.Dom
 
-main = mainWidget $ mdo 
-    -- create an element ("div") that shows the clickCount
+clicker = mdo 
     (element,_) <- el' "div" $ display nClicks
-
-    let 
-        -- an Event that fires whenever the element is clicked
-        clickEvent = domEvent Click element    
-
-        -- a Dynamic (Behavior + Event) that counts clicks
-        clickCount = count clickEvent   
-
-    -- extract nClicks from the Reflex monad.
-    nClicks <- clickCount                
-
+    nClicks <- count $ domEvent Click element    
     return ()
+
+main = mainWidget $ do
+    clicker
 

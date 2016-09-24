33a675c0a6393b3cb9138384b3930036e746784b
Dave Compton <davecompton7@gmail.com>
2016-09-24 00:21:14 +0000

    added type signature for clicker and using foldDyn instead of count

--- Main.hs
+++ Main.hs
@@ -2,11 +2,14 @@
 {-# LANGUAGE RecursiveDo #-}
 import Reflex.Dom
 
-clicker = mdo 
+
+clicker :: MonadWidget t m => m ()
+clicker = mdo
     (element,_) <- el' "div" $ display nClicks
-    nClicks <- count $ domEvent Click element    
+    nClicks <- foldDyn (\_ -> succ) (0::Integer) $ domEvent Click element    
     return ()
 
 main = mainWidget $ do
+    clicker
     clicker
 
